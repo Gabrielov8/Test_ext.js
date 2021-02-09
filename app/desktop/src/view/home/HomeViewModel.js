818eae28f4808e04c390ff00c4045cce1d5d268a
Габриелов Максим <gabrielovma@edu.mos.ru>
2021-02-09 13:39:55 +0300

    first commit

---
+++ app/desktop/src/view/home/HomeViewModel.js
@@ -0,0 +1,8 @@
+Ext.define('Fashboard.view.home.HomeViewModel', {
+	extend: 'Ext.app.ViewModel',
+	alias: 'viewmodel.homeviewmodel',
+	data: {
+		name: 'homeview'
+	},
+
+});
---
+++ app/shared/src/view/Readme.md
@@ -0,0 +1,1 @@
+# Fashboard shared/app
